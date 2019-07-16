5823ea04ade067d44b806c29d98f078701725b2b
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-07-16 15:07:11 -0400

    Update AD attribute formatting and added user existence check

--- create_ad_user.py
+++ create_ad_user.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # Jason Satti
+
+import re
+
 from ldap3 import Server, Connection, ALL
 
 
@@ -11,10 +14,10 @@ def set_ad_server_info():
                     get_info=ALL)
 
     # Set DC
-    dc = ',cn=Users,dc=hostname,dc=dc,dc=dc'
+    dc = 'cn=Users,dc=hostname,dc=dc,dc=dc'
 
     # Set domain user info
-    domain_user = 'cn=domain.user' + dc
+    domain_user = 'cn=domain.user' + ',' + dc
     password = 'password'
 
     return server, domain_user, password, dc
@@ -25,37 +28,40 @@ def get_ad_user_info(dc):
 
     # Request new AD user information
     employee_full_name = input('What is the new employee\'s name? (format '
-                               'first.last): ').lower()
+                               'First Last): ')
     employee_manager_name = input(
         'Who is the new employee\'s manager? (format '
-        'first.last): ').lower()
+        'First Last): ').lower()
     employee_title = input('What is the new employee\'s job title? ').title()
     employee_department = input('What is the new employee\'s department? '
                                 '').title()
-    employee_manager = 'cn=' + employee_manager_name + dc
-    employee_name = employee_full_name.split('.')
+    employee_manager = 'cn=' + employee_manager_name.replace(' ', '.') + ',' \
+                                                                         '' + dc
+    employee_name = employee_full_name.split(' ')
     employee_first_name = employee_name[0]
     employee_last_name = employee_name[1]
-    employee_display_name = employee_first_name.capitalize() + " " + \
-                            employee_last_name.capitalize()
-    employee_email = employee_full_name + '@company.com'
-    new_employee = 'cn=' + employee_full_name + dc
+    employee_first_adname = re.sub('\W', '', employee_first_name.lower())
+    employee_last_adname = re.sub('\W', '', employee_last_name.lower())
+    employee_full_adname = employee_first_adname + '.' + employee_last_adname
+    employee_email = employee_full_adname + '@company.com'
+    new_employee = 'cn=' + employee_full_adname + ',' + dc
 
     # Set up new ad user attributes
     attrs = {
         "objectClass": ['user', 'organizationalPerson', 'person', 'top'],
         "givenname": employee_first_name,
         "sn": employee_last_name,
-        "cn": employee_full_name,
-        "displayName": employee_display_name,
-        "samaccountname": employee_full_name,
+        "cn": employee_full_adname,
+        "displayName": employee_full_name,
+        "samaccountname": employee_full_adname,
+        "userprincipalname": employee_full_adname,
         "mail": employee_email,
         "manager": employee_manager,
         "title": employee_title,
         "department": employee_department
     }
 
-    return new_employee, attrs
+    return new_employee, attrs, employee_full_adname
 
 
 def main():
@@ -71,8 +77,12 @@ def main():
     conn = ldap_connector()
 
     # Add new ad user
-    new_employee, attrs = get_ad_user_info(dc)
-    conn.add(new_employee, attributes=attrs)
+    new_employee, attrs, employee_full_adname = get_ad_user_info(dc)
+    if not conn.search(dc, '(&(objectCategory=person)'
+    F'(sAMAccountName={employee_full_adname}))'):
+        conn.add(new_employee, attributes=attrs)
+    else:
+        print('Employee already exists in Active Directory')
 
     # Update user password
     conn.extend.microsoft.modify_password(new_employee, 'new.user1')
