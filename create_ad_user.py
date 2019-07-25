d8dd265c917941b2de00e8fb64233afd0cb9e6de
Jason Satti <48844684+JasonSatti@users.noreply.github.com>
2019-07-25 15:02:25 -0400

    Add CLI arguments for AD username/password
    Add an optional '-ru/--remember' flag to remember AD username locally.

--- create_ad_user.py
+++ create_ad_user.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 # Jason Satti
 
+import argparse
+import os
 import re
 
 from ldap3 import Server, Connection, ALL
 
 
-def set_ad_server_info():
-    """Set the Active Directory Domain server information"""
+def set_ad_server_info(ad_username, ad_password):
+    """Set the Active Directory Domain server information.
+
+    :param ad_username: Passed in via CLI or ad_info file
+    :param ad_password: Passed in via CLI
+    """
 
     # Set domain server
     server = Server('ldaps://hostname.dc.dc', port=636, use_ssl=True,
@@ -17,14 +23,29 @@ def set_ad_server_info():
     dc = 'cn=Users,dc=hostname,dc=dc,dc=dc'
 
     # Set domain user info
-    domain_user = 'cn=domain.user' + ',' + dc
-    password = 'password'
+    domain_user = F'cn={ad_username}' + ',' + dc
+    password = ad_password
 
     return server, domain_user, password, dc
 
 
+def save_domain_username(username):
+    """Save AD domain admin username to file.
+
+    :param username: Passed in via CLI
+    """
+
+    username_template = F"""username='{username}'\n"""
+    with os.fdopen(os.open('ad_info.py', os.O_WRONLY | os.O_CREAT, 0o600),
+                   'w') as F:
+        F.write(username_template)
+
+
 def get_ad_user_info(dc):
-    """Prompt for new AD user information"""
+    """Prompt for new AD user information.
+
+    :param dc: Generated via set_ad_server_info() and passed in.
+    """
 
     # Request new AD user information
     employee_full_name = input('What is the new employee\'s name? (format '
@@ -32,11 +53,13 @@ def get_ad_user_info(dc):
     employee_manager_name = input(
         'Who is the new employee\'s manager? (format '
         'First Last): ').lower()
-    employee_title = input('What is the new employee\'s job title? ').title()
+    employee_title = input(
+        'What is the new employee\'s job title? ').title()
     employee_department = input('What is the new employee\'s department? '
                                 '').title()
-    employee_manager = 'cn=' + employee_manager_name.replace(' ', '.') + ',' \
-                                                                         '' + dc
+    employee_manager = 'cn=' + employee_manager_name.replace(' ',
+                                                             '.') + ',' \
+                                                                    '' + dc
     employee_name = employee_full_name.split(' ')
     employee_first_name = employee_name[0]
     employee_last_name = employee_name[1]
@@ -54,7 +77,7 @@ def get_ad_user_info(dc):
         "cn": employee_full_adname,
         "displayName": employee_full_name,
         "samaccountname": employee_full_adname,
-        "userprincipalname": employee_full_adname,
+        "userprincipalname": employee_full_adname + '@hostname.dc.com',
         "mail": employee_email,
         "manager": employee_manager,
         "title": employee_title,
@@ -65,10 +88,32 @@ def get_ad_user_info(dc):
 
 
 def main():
-    """Connect to AD domain and create new user entry"""
+    """Connect to AD domain and create new user entry."""
+
+    # Get AD Domain user info via CLI
+    parser = argparse.ArgumentParser(description='Connect to AD Account')
+    parser.add_argument('-un', '--username', required=False, type=str,
+                        help='Username of AD domain admin in the format '
+                             'fist.last. '
+                             'Mandatory on first run, optional after if '
+                             '-ru/--remember flag is used.')
+    parser.add_argument('-pw', '--password', required=True, type=str,
+                        help='Password of AD domain admin.')
+    parser.add_argument('-ru', '--remember', action='store_true',
+                        default=False,
+                        help='Save AD domain admin username locally.')
+    args = parser.parse_args()
+
+    if args.remember is True:
+        save_domain_username(args.username)
+
+    if os.path.exists('./ad_info.py'):
+        import ad_info
+        args.username = ad_info.username
 
     # Set up connector
-    server, domain_user, password, dc = set_ad_server_info()
+    server, domain_user, password, dc = set_ad_server_info(args.username,
+                                                           args.password)
     ldap_connector = lambda: Connection(server, domain_user, password,
                                         return_empty_attributes=True,
                                         auto_bind=True)
@@ -76,13 +121,16 @@ def main():
     # Open a connection
     conn = ldap_connector()
 
+    # Get new ad user info
+    new_employee, attrs, employee_full_adname = get_ad_user_info(dc)
+
     # Check if ad user exists
     if not conn.search(dc, '(&(objectCategory=person)'
     F'(sAMAccountName={employee_full_adname}))'):
         # Add new ad user
         conn.add(new_employee, attributes=attrs)
-        # Update new ad user password
-        conn.extend.microsoft.modify_password(new_employee, 'password')
+        # Set new ad user password
+        conn.extend.microsoft.modify_password(new_employee, 'default password')
         # Enable new ad user account
         conn.modify(new_employee,
                     {'userAccountControl': [('MODIFY_REPLACE', 512)]})
